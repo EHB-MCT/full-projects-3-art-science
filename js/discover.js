c9412dac8957deab306cf53f22d1aecfa57cec1c
IlhanKurultay <91081941+IlhanKurultay@users.noreply.github.com>
2023-01-25 11:58:10 +0100

    discover works with collections + css

---
+++ js/discover.js
@@ -0,0 +1,26 @@
+const btnArtworks = document.getElementById("btn-artwork");
+const btnCollection = document.getElementById("btn-collection");
+
+const artworksBlock = document.getElementById("artworks");
+const collectionsBlock = document.getElementById("collections");
+btnArtworks.addEventListener("click", domArtworks);
+btnCollection.addEventListener("click", domCollection);
+
+
+
+function domCollection() {
+    document.getElementById("artworks").style.display = "none";
+    document.getElementById("collections").style.display = "block";
+}
+
+function domArtworks() {
+    document.getElementById("artworks").style.display = "block";
+    document.getElementById("collections").style.display = "none";
+}
+
+let x = Math.floor(Math.random() * 6) + 1;
+
+let image = ["one", "two", "three"]
+
+console.log(x);
+console.log(image[x]);
