94a15311a974244013a206698cc26a197e07e8e6
JensErven <91070027+JensErven@users.noreply.github.com>
2023-01-31 16:38:03 +0100

    WIP rendering collections on profile page

---
+++ frontend/js/profile.js
@@ -0,0 +1,48 @@
+let userId = JSON.parse(sessionStorage.getItem('user'));
+
+getData(`http://localhost:3000/getCollectionsByUserID?id=${userId.userId}`)
+    .then(data => {
+        renderCollections(data.data);
+
+    })
+
+
+async function getData(url) {
+    try {
+        let resp = await fetch(url, {
+            headers: {
+                'Content-Type': "application/json"
+            },
+        });
+        const json = await resp.json();
+        return json
+
+    } catch (error) {}
+}
+
+function renderCollections(data) {
+    let htmlString = "";
+    console.log(data)
+    data.forEach(item => {
+        var input = item.listOfArtworks[2].toString();
+        var result = [];
+        console.log(result)
+
+        while (input.length) {
+            result.push(input.substr(0, 4));
+            input = input.substr(4);
+        }
+
+        htmlString += `
+       <div class="card">
+        <a href="mycollection.html"><img
+                src="https://kunstinhuis.be/assets/files/artworks/_grid/${result[0]}_${result[1]}.jpg"
+                alt=""></a>
+        <h3>${item.collectionName}</h3>
+        <p>Van Mijzelf</p>
+    </div>
+       `;
+    });
+    let container = document.getElementById("profile-collections");
+    container.insertAdjacentHTML("afterbegin", htmlString);
+}
